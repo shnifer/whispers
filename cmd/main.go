4100d17b931722c9689ebcb945f2141c3802b025
shnifer <phoeni_ks@mail.ru>
2019-09-07 00:52:52 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shnifer/whispers/hello"
+)
+
+func main() {
+	fmt.Println("Heelo")
+	fmt.Println(hello.Hello())
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/shnifer/whispers
+
+go 1.13
+
+require (
+	golang.org/x/text v0.3.0 // indirect
+	rsc.io/quote v1.5.2
+)
